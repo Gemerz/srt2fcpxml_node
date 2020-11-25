68cd76cd0429d2cb6b23d489ebbc5000c635772d
chloe <chloecn8@gmail.com>
2020-11-25 16:21:54 +0800

    add rate and checker

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ export * from './lib/number';
 import * as root from 'app-root-path';
 import { program } from 'commander';
 
-import { resourcesEnum } from './constants/resources';
-import { checkSrtPath } from './lib/helper';
+import { resources, resourcesEnum } from './constants/resources';
+import { checkRate, checkSrtPath, formateRateKey } from './lib/helper';
 import { Parser } from './parser';
 
 program
@@ -19,12 +19,9 @@ program.parse(process.argv);
 if (program.opts().srt) {
   const srtParam = program.opts().srt;
   const outputParam = program.opts().output || root.path;
-  const rateParam = program.opts().rate || resourcesEnum.p2400;
+  const rateParam = formateRateKey(program.opts().rate);
   const eventName = program.opts().event || 'srt2fcpxml_node';
-  for (let i in resourcesEnum) {
-    console.log(i)
-  }
-  if (checkSrtPath(srtParam)) {
+  if (checkSrtPath(srtParam) && checkRate(rateParam)) {
     Parser({
       srtPath: srtParam,
       outputPath: outputParam,
--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -4,6 +4,8 @@
 import chalk from 'chalk';
 import * as fs from 'fs-extra';
 
+import { resources, resourcesEnum } from '../constants/resources';
+
 const log = console.log;
 export const checkSrtPath = (srtPath: string): boolean => {
   if (!fs.existsSync(srtPath)) {
@@ -14,6 +16,17 @@ export const checkSrtPath = (srtPath: string): boolean => {
 };
 
 export const checkRate = (rate: string): boolean => {
-  console.log('rate', rate);
+  const keys = Object.keys(resourcesEnum)
+  if (!keys.includes(rate)) {
+    log(chalk.red("the input rate doesn't match, example: 23.98,24,25,29.87,30,50,59.94,60"));
+    return false
+  }
   return true;
+};
+export const formateRateKey = (rate: string): string => {
+  if (rate) {
+    return 'p' + (parseFloat(rate) * 100).toString();
+  } else {
+    return resourcesEnum.p3000
+  }
 };
